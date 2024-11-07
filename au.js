dfd451aa2d1ebd48b8e4c16e751ff53c91659668
airtsar <161482851+airtsar@users.noreply.github.com>
2024-11-07 14:40:40 +0700

    Create au.js

---
+++ au.js
@@ -0,0 +1,87 @@
+(function() {
+    'use strict';
+
+    const keys = ['airtsar_set', 'protocol', 'helper'];
+    let previousSettings = {};
+
+    // Добавляем поле для ввода токена в настройки интерфейса
+    Lampa.SettingsApi.addParam({
+        component: 'interface',
+        param: {
+            name: 'user_token',
+            type: 'input',
+            default: '',
+        },
+        field: {
+            name: 'Токен пользователя',
+            description: "Введите уникальный токен для синхронизации настроек",
+        },
+        onRender: function(item) {
+            setTimeout(function() {
+                const tokenElement = $('div[data-name="user_token"]');
+                if (tokenElement.length) {
+                    tokenElement.insertAfter('div[data-name="logo_card"]');
+                }
+            }, 0);
+        },
+        onChange: function(item) {
+            const token = item.value;
+            if (token) {
+                console.log('Token введен:', token);
+                Lampa.Storage.set('user_token', token);
+            }
+        }
+    });
+
+    function trackChangesAndSync() {
+        const token = Lampa.Storage.get('user_token');
+        if (!token) {
+            console.log('Токен не введен. Ожидание...');
+            return;
+        }
+
+        const currentSettings = {};
+        keys.forEach(key => {
+            const value = Lampa.Storage.get(key);
+            if (value !== undefined) {
+                currentSettings[key] = value;
+            }
+        });
+
+        let hasChanges = false;
+        for (let key in currentSettings) {
+            if (currentSettings[key] !== previousSettings[key]) {
+                hasChanges = true;
+                break;
+            }
+        }
+
+        if (hasChanges) {
+            console.log('Изменения обнаружены, отправка данных на сервер');
+
+            fetch('https://yourserver.com/save_settings', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    token: token,
+                    settings: currentSettings,
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Настройки сохранены на сервере:', data);
+                previousSettings = { ...currentSettings };
+            })
+            .catch(error => {
+                console.error('Ошибка при отправке данных:', error);
+            });
+        } else {
+            console.log('Нет изменений, синхронизация не требуется');
+        }
+    }
+
+    setInterval(trackChangesAndSync, 5 * 60 * 1000);
+
+})();
